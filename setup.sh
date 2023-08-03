c89fde5882370e7452b507be2db599e86988d356
John Miller <jbmiller10@gmail.com>
2023-08-03 01:53:14 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -47,7 +47,7 @@ CMAKE_ARGS="-DLLAMA_CUBLAS=on"
 FORCE_CMAKE=1
 pip install llama-cpp-python --force-reinstall --upgrade --no-cache-dir --verbose
 if [ -f "requirements.txt" ]; then
-    pip install -r requirements.txt --force-reinstall --upgrade --no-cache-dir
+    pip install -r requirements.txt --force-reinstall 
     
 fi
 deactivate
